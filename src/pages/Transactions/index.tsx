0bf50a0f621ea7da30f4f705aaa8a2fa64724d47
Joel Luis <joelluis73@gmail.com>
2022-10-27 19:39:33 -0300

    create component table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from 'components/Header'
 import { Summary } from 'components/Summary'
 
+import * as S from './styles'
+
 export function Transactions() {
   return (
-    <div>
+    <>
       <Header />
       <Summary />
-    </div>
+      <S.TransactionsContainer>
+        <S.TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <S.PriceHighlight variant="income">
+                  R$ 12.000,00
+                </S.PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>21/09/2022</td>
+            </tr>
+            <tr>
+              <td width="40%">Hamburguer</td>
+              <td>
+                <S.PriceHighlight variant="outcome">
+                  - R$ 59,00
+                </S.PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/09/2022</td>
+            </tr>
+          </tbody>
+        </S.TransactionsTable>
+      </S.TransactionsContainer>
+    </>
   )
 }
