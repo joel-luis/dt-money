c110c2b837630739b08551284638a78e7e0ee19c
Joel Luis <joelluis73@gmail.com>
2022-10-27 21:09:52 -0300

    create component modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
 import * as S from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
+
 import igniteLogo from 'assets/ignite-logo.svg'
+import { NewTransactionModal } from 'components/NewTransactionModal'
 
 export function Header() {
   return (
     <S.HeaderContainer>
       <S.HeaderContent>
         <img src={igniteLogo} alt="" />
-        <S.NewTransactionButton>Nova transação</S.NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <S.NewTransactionButton>Nova transação</S.NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
       </S.HeaderContent>
     </S.HeaderContainer>
   )
